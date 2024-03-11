e15ad5bc1dfc8c8db0a6ca0fb21fa760a38339e5
matematiflo <7537413+matematiflo@users.noreply.github.com>
2024-03-11 07:35:20 -0500

    Refactor isEven to Nat.isEven

--- FormalMath/LosAndes2024.lean
+++ FormalMath/LosAndes2024.lean
@@ -9,11 +9,11 @@ theorem Coloquio2024 : ∀ (n m : Nat),
       intro n m t
       cases' t with h1 h2
       · cases' h1 with k1 p1
-        unfold isEven
+        unfold Nat.isEven
         use (k1 * m)
         rw [p1, Nat.mul_assoc]
       · cases' h2 with k2 p2
-        unfold isEven
+        unfold Nat.isEven
         use (n * k2)
         rw [p2, ← Nat.mul_assoc, Nat.mul_comm n 2, Nat.mul_assoc]
     }
